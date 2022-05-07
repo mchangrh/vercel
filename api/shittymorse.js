8d3fe2a9f94aea77b195effb755f9dd6b4509d27
Michael C <michael@mchang.name>
2022-05-06 23:37:27 -0400

    add shittymorse

---
+++ api/shittymorse.js
@@ -0,0 +1,14 @@
+/*
+  Morse code, but with en and em dashes instead of dots and dashes.
+
+  Please don't kill me
+*/
+export default function handler(request, response) {
+  const text = request.body;
+  if (!text) {
+    return response.status(400).end("Please include text in the request body")
+  }
+  const morse = text.replace(/\./gi, '–')
+    .replace(/-/gi, '—')
+  response.end(morse);
+}
